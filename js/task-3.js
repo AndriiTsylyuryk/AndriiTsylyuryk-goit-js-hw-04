fe9283e883648760f2287f6bc67c3d9dd22393df
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-04-06 12:06:35 +0300

    b4 lms

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,26 @@
 "use strict";
 
-function filterArray(numbers, value) {
-  const arr = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      arr.push(numbers[i]);
-    }
-  }
-  return arr;
-}
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-// function checkForSpam(message) {
-//   message = message.toLowerCase();
-//   if (message.includes("spam") || message.includes("sale")) return true;
-//   else { return false }
-// }
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-// console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-// console.log(checkForSpam("Trust me, this is not a spam message")); // true
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
